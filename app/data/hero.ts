b17f13e67e54a22700e9a8bb2fdc51d32db55f5c
Baixerastheo <theo.baixeras@gmail.com>
2025-11-23 15:59:40 +0100

    Modif picture

--- app/data/hero.ts
+++ app/data/hero.ts
@@ -15,6 +15,6 @@ export const heroData: HeroData = {
   description: "Passionné par la création d'expériences web innovantes et performantes. Je transforme vos idées en applications web élégantes et fonctionnelles.",
   ctaText: "Mon curriculum vitae",
   ctaLink: "/projects",
-  image: "/pf_img.png"
+  image: "/me.profile.jpg"
 }
 
